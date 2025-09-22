6a7299d68070b5b85be88f4e0c143dd8783dc4f0
kbstudent <trevel37.fifa@gmail.com>
2025-09-22 09:36:29 +0300

    добавляем класс "продукты" и расписываем его атрибуты

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -10,5 +10,14 @@ namespace ShopInventory
         Одежда,
         Продукты
     }
+public class Product
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int Quantity { get; private set; }
+        public bool IsAvailable => Quantity > 0;
+        public Category Category { get; }
 
+    }
 }
