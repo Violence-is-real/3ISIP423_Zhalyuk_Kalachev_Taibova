a5389a3fe746216566caf34e6e655f8eaca4e578
234867 <trevel37.fifa@gmail.com>
2025-09-30 12:51:43 +0300

    Создаем структуру данных

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -12,4 +12,20 @@ namespace LibraryApp
         Romance,
         Horror
     }
+    public class Book
+    {
+        private static int nextId = 1;
+
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public Book()
+        {
+            Id = nextId++;
+        }
+    }
 }
