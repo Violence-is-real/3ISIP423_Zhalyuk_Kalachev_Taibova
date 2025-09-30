23f50b564936b83649639198c2f52b757218001e
234866 <trevel37.fifa@gmail.com>
2025-09-30 11:02:34 +0300

    Создаем списки для: хранения всей статистики, глассных и согласных букв

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -16,3 +16,20 @@ class TextStatistics
     public DateTime ProcessedAt { get; set; }
 }
 
+class Program
+{
+    // Список для хранения статистики по всем текстам
+    private static List<TextStatistics> allStatistics = new List<TextStatistics>();
+
+    // Множества гласных и согласных букв (русский и английский алфавиты)
+    private static HashSet<char> vowels = new HashSet<char>
+{
+'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
+'a', 'e', 'i', 'o', 'u', 'y'
+};
+
+    private static HashSet<char> consonants = new HashSet<char>
+{
+'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
+};
