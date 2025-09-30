dc83773964c485bea5bde83cec435134493fe7e2
234867 <trevel37.fifa@gmail.com>
2025-09-30 12:49:53 +0300

    Добавляем пространство и добавляем жанры книг

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryApp
+{
+    public enum Genre
+    {
+        Fantasy,
+        ScienceFiction,
+        Mystery,
+        Romance,
+        Horror
+    }
+}
