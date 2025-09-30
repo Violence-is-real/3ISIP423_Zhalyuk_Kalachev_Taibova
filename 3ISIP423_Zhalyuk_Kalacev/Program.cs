15bbf2b8d5a41c2591f5d66ffd6b704afea88dee
234866 <trevel37.fifa@gmail.com>
2025-09-30 11:05:12 +0300

    создаем меню программы

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -33,3 +33,39 @@ class Program
 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
 };
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        while (true)
+        {
+            Console.WriteLine("\nАнализатор текста");
+            Console.WriteLine("1. Анализ нового текста");
+            Console.WriteLine("2. Просмотр статистики по прошлым текстам");
+            Console.WriteLine("3. Выход");
+            Console.Write("Выберите действие: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AnalyzeNewText();
+                    break;
+                case "2":
+                    ShowPastStatistics();
+                    break;
+                case "3":
+                    Console.WriteLine("До свидания!");
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор!");
+                    break;
+            }
+        }
+    }
+}
+
+
