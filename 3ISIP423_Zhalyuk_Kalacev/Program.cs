9e20ebf8fdc70fa357c90e64e0615ffaf8a2be9e
234866 <trevel37.fifa@gmail.com>
2025-09-30 10:57:04 +0300

    Подключаем библиотеки и создаем класс для хранения статистики

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Collections.Generic;
+
+class TextStatistics
+{
+    public string Text { get; set; }
+    public int WordCount { get; set; }
+    public string ShortestWord { get; set; }
+    public int SentenceCount { get; set; }
+    public int VowelCount { get; set; }
+    public int ConsonantCount { get; set; }
+    public string LongestWord { get; set; }
+    public Dictionary<char, int> LetterFrequency { get; set; }
+    public DateTime ProcessedAt { get; set; }
+}
+
