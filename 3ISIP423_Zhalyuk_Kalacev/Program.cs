c629cf81d540b65f89a24af205a493cd4a4a1304
234866 <trevel37.fifa@gmail.com>
2025-10-07 11:43:07 +0300

    подключаем бибилиотеки и добавляем первый класс "человек" и делаем его абстрактным

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public abstract class Person
+{
+    public int Id { get; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+
+    protected Person(int id, string name, int age, string email)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+
+    public abstract string GetInfo();
+}
