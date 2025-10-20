59690620903f352108f571e93f2fb592aa8045b7
234866 <trevel37.fifa@gmail.com>
2025-10-20 10:47:16 +0300

    создаем класс Врагов и пишем характеристики класса

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -28,3 +28,29 @@ public class Armor : Item
 {
     public Armor(string name, int defense) : base(name, 0, defense) { }
 }
+public class Enemy
+{
+    public string Name { get; set; }
+    public int MaxHP { get; set; }
+    public int HP { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public double CritChance { get; set; }
+    public double FreezeChance { get; set; }
+    public bool IgnoreDefense { get; set; }
+
+    public Enemy(string name, int hp, int attack, int defense,
+                double critChance = 0, double freezeChance = 0, bool ignoreDefense = false)
+    {
+        Name = name;
+        MaxHP = hp;
+        HP = hp;
+        Attack = attack;
+        Defense = defense;
+        CritChance = critChance;
+        FreezeChance = freezeChance;
+        IgnoreDefense = ignoreDefense;
+    }
+
+    public bool IsAlive() => HP > 0;
+}
