4708c1b5951dd7a832a228120460428f20607a71
234866 <trevel37.fifa@gmail.com>
2025-10-20 10:17:27 +0300

    создаем пространство с именем рогалик исоздаемтам класс item

--- 3ISIP423_Zhalyuk_Kalacev/Program.cs
+++ 3ISIP423_Zhalyuk_Kalacev/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextRoguelike
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        public Item(string name, int attack = 0, int defense = 0)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+    }
+}
